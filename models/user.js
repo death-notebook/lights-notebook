674cf30593b9a9cd911dd3dc53ad66da3b90da91
YaredBG <Yared_Gari@Mckinsey.com>
2022-08-16 17:41:00 -0400

    updated and tested user model and routes code

--- models/user.js
+++ models/user.js
@@ -1,24 +1,37 @@
-
-
 module.exports = (sequelize,DataTypes)=>{
-    const User = sequelize.define("user", {
+    const User = sequelize.define("User", {
         userId:{
             type:DataTypes.STRING ,
-            defaultValue: Date.now().toString()
-            //allowNull:false
+            defaultValue: Date.now().toString(),
+            allowNull:false
         },
         username:{
             type:DataTypes.STRING,
-            //allowNull:false
+            allowNull:false
+        },
+        email:{
+            type:DataTypes.STRING,
+            isEmail: true,
+            //allowNull:false,
         },
         password:{
             type:DataTypes.STRING,
-            //allowNull:false
+            allowNull:false
         },
         role:{
-            type:DataTypes.ENUM('admin', 'user'),
-            //allowNull:false
+            type: DataTypes.ENUM("user", "admin"),
+            defaultValue: "user",
+            allowNull:false
         }
-    })
+    });
+
+    User.associate = (models)=>{
+        User.hasMany(models.People, {
+            onDelete: 'cascade',
+            foreignKey:'userId',
+            as:"createdby"
+        })
+    }
+
   return User
 };
